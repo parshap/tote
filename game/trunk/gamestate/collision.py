329bb839c00affc7dda7521d21fd96e143326012
Preston Lewis <prestonclewis@gmail.com>
2009-04-29 11:43:03 +0000

    BoundingShape objects no longer contain a position attribute; instead, the position must be explicitly passed to CollisionDetector.checkCollision(). See method signature for details.

--- game/trunk/gamestate/collision.py
+++ game/trunk/gamestate/collision.py
@@ -8,13 +8,12 @@ class BoundingShape:
 class BoundingCircle(BoundingShape):
     def __init__(self, radius):
         self.radius = radius
-        self.position = ogre.vector3(0,0,0)
         self.shapeType = "circle"
 
 class BoundingLineSegment(BoundingShape):
     def __init__(self, point1, point2, normal):
-        self.point1 = point1
-        self.point2 = point2
+        self.position = point1
+        self.vector = ogre.vector3(point1.x - point2.x, point1.y - point2.y, point1.z - point2.z)
         self.normal = normal
         self.shapeType = "linesegment"
         
@@ -22,24 +21,28 @@ class CollisionDetector:
     def castRay(originPoint, orientation):
         pass
     
-    def checkCollision(colliderShape, collideeShape):
+    def checkCollision(colliderShape, colliderShapePosition, collideeShape, collideeShapePosition):
         # determine type of shape and call appropriate helper function
         if(colliderShape.shapeType == "circle" and collideeShape == "linesegment"):
-            return _check_circle_segment_collision(colliderShape, collideeShape)
+            return _check_circle_segment_collision(colliderShape, colliderShapePosition, collideeShape, collideeShapePosition)
         pass
     
-    def _check_circle_segment_collision(segment, circle):
+    def _check_circle_segment_collision(segment, segmentPosition, circle, circlePosition):
+        # get the absolute position of the line segment vertices
+        point1 = segment.position
+        point2 = segment.position + segment.vector
+        
         # first transform the segment vertices to coordinates relative to the circle's center
-        localP1 = ogre.Vector3(segment.point1.x - circle.position.x, 0, segment.point1.z - circle.position.z)
-        localP2 = ogre.Vector3(segment.point2.x - circle.position.x, 0, segment.point2.z - circle.position.z)
+        localP1 = ogre.Vector3(point1.x - circlePosition.x, 0, point1.z - circlePosition.z)
+        localP2 = ogre.Vector3(point2.x - circlePosition.x, 0, point2.z - circlePosition.z)
         
         # pre-calculate p1-p2 for easy reference
-        p2Minusp1 = ogre.Vector3(segment.point2.x - segment.point1.x, 0, segment.point2.z - segment.point1.z)
+        p2Minusp1 = ogre.Vector3(point2.x - point1.x, 0, point2.z - point1.z) # same as segment.vector?
         
         # get quadratic coefficients
         a = (p2Minusp1.x * p2Minusp1.x) + (p2Minusp1.z * p2Minusp1.z)
         b = 2 * ((p2Minusp1.x * localP1.x) + (p2Minusp1.z * localP1.z))
-        c = (localP1.x * localP1.x) + (localP1.z * localP1.z) - (circleRadius * circleRadius)
+        c = (localP1.x * localP1.x) + (localP1.z * localP1.z) - (circle.radius * circle.radius)
         
         discrim = b * b - 4 * a * c
         
@@ -49,7 +52,7 @@ class CollisionDetector:
         elif (discrim == 0): # perfect collision
             # u is the % of the distance from p1 to p2 the intersection point falls at
             u = -b / (2 * a)
-            collisionPoint = (segment.point1.x + u * p2Minusp1.x, 0, segment.point1.z + u * p2Minusp1.z)
+            collisionPoint = (point1.x + u * p2Minusp1.x, 0, point1.z + u * p2Minusp1.z)
             if(u < 0 or u > 1):
                 return None
             else:
@@ -64,8 +67,8 @@ class CollisionDetector:
                 return None
             
             # now we calculate the two collision points (where the circle intersects the segment
-            intersectionPoint1 = ogre.Vector3(segment.point1.x + u1 * p2Minusp1.x, 0, segment.point1.z + u1 * p2Minusp1.z)
-            intersectionPoint2 = ogre.Vector3(segment.point1.x + u2 * p2Minusp1.x, 0, segment.point1.z + u2 * p2Minusp1.z)
+            intersectionPoint1 = ogre.Vector3(point1.x + u1 * p2Minusp1.x, 0, point1.z + u1 * p2Minusp1.z)
+            intersectionPoint2 = ogre.Vector3(point1.x + u2 * p2Minusp1.x, 0, point1.z + u2 * p2Minusp1.z)
             
             # find the point on the segment where the circle collided initially
             collisionPoint = intersectionPoint1.midPoint(intersectionPoint2)
