027421ce19289135b992b0e3feccb3eab6c8eddb
Parsha Pourkhomami <parshap+git@gmail.com>
2009-05-03 02:38:03 +0000

    Passing BoundingLineSegment tuples instead of ogre.Vector3s.

--- game/branches/collision/playscene.py
+++ game/branches/collision/playscene.py
@@ -102,30 +102,30 @@ class PlayScene(ogre.FrameListener, ogre.WindowEventListener):
         boundary1 = gamestate.objects.GameObject(self.world)
         boundary1.position = (-90, -90)
         boundary1.isPassable = False
-        boundary1.bounding_shape = gamestate.collision.BoundingLineSegment(ogre.Vector3(-90, 0, -90),
-                                                                           ogre.Vector3(90, 0, -90),
-                                                                           ogre.Vector3(0, 0, 1))
+        boundary1.bounding_shape = gamestate.collision.BoundingLineSegment((-90, -90),
+                                                                           (90, -90),
+                                                                           (0, 1))
         # south wall
         boundary2 = gamestate.objects.GameObject(self.world)
         boundary2.position = (-90, 90)
         boundary2.isPassable = False
-        boundary2.bounding_shape = gamestate.collision.BoundingLineSegment(ogre.Vector3(-90, 0, 90),
-                                                                           ogre.Vector3(90, 0, 90),
-                                                                           ogre.Vector3(0, 0, -1))
+        boundary2.bounding_shape = gamestate.collision.BoundingLineSegment((-90, 90),
+                                                                           (90, 90),
+                                                                           (0, -1))
         # east wall
         boundary3 = gamestate.objects.GameObject(self.world)
         boundary3.position = (90, -90)
         boundary3.isPassable = False
-        boundary3.bounding_shape = gamestate.collision.BoundingLineSegment(ogre.Vector3(90, 0, -90),
-                                                                           ogre.Vector3(90, 0, 90),
-                                                                           ogre.Vector3(-1, 0, 0))
+        boundary3.bounding_shape = gamestate.collision.BoundingLineSegment((90, -90),
+                                                                           (90, 90),
+                                                                           (-1, 0))
         # west wall
         boundary4 = gamestate.objects.GameObject(self.world)
         boundary4.position = (-90, -90)
         boundary4.isPassable = False
-        boundary4.bounding_shape = gamestate.collision.BoundingLineSegment(ogre.Vector3(-90, 0, -90),
-                                                                           ogre.Vector3(-90, 0, 90),
-                                                                           ogre.Vector3(1, 0, 0))
+        boundary4.bounding_shape = gamestate.collision.BoundingLineSegment((-90, -90),
+                                                                           (-90, 90),
+                                                                           (1, 0))
         self.world.add_object(boundary1)
         self.world.add_object(boundary2)
         self.world.add_object(boundary3)
