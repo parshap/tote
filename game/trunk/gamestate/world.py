450f144c740e6586c9fec448b610d08d4946b440
Parsha Pourkhomami <parshap+git@gmail.com>
2009-05-06 01:45:21 +0000

    Implemented world method get_colliders() that takes a bounding shape and position of that shape and returns a list of game objects that are colliding with that shape at the given position. Implemented for immediate use in ability implementations.

--- game/trunk/gamestate/world.py
+++ game/trunk/gamestate/world.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from collision import CollisionDetector
 
 from event import Event
 
@@ -26,3 +27,29 @@ class World(object):
     def log(self, text):
         self.debug_file.write(text)
         self.debug_file.write("\n")
+        
+    def get_colliders(self, bounding_shape, position, ignored=[]):
+        """
+        Returns a list of game objects in the world that are currently
+        colliding with the given bounding_shape at the given position. This
+        method performs no collision resolution.
+        
+        This method is useful for collision between game objects in the world
+        and some bounding shape that may not be "in" the world (e.g., a
+        temporary bounding shape used for abilities).
+        
+        Arguments:
+        bounding_shape -- The shape to check objects against.
+        position -- The position of the shape to check objects against.
+        ignored -- A list of game objects to ignore collision with.
+        """
+        colliders = []
+        for object in self.objects:
+            if object.bounding_shape is None:
+                # Can't collide with an object that has no bounding shape.
+                continue
+            result = CollisionDetector.check_collision(bounding_shape, position, object.bounding_shape, object.position)
+            if result is not None and object not in ignored:
+                colliders.append(object)
+                
+        return colliders
