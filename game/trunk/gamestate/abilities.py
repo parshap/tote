34689694b6092aa9704e620970cbc424f2d8ba24
Parsha Pourkhomami <parshap+git@gmail.com>
2009-05-15 05:14:33 +0000

    Changed earth primary melee to use bounding cone instead of circle.

--- game/trunk/gamestate/abilities.py
+++ game/trunk/gamestate/abilities.py
@@ -1,6 +1,8 @@
 #from event import Event
 import collision
 from collision import CollisionDetector
+import objects
+import math
 
 class AbilityInstance(object):
     def __init__(self, player):
@@ -19,7 +21,11 @@ class EarthPrimaryInstance(AbilityInstance):
         # bigger than the player's ignoring the player that is using the ability.
         # @todo: use real collision using cone(?)
         # @todo: have swing delay
-        colliders = self.player.world.get_colliders(collision.BoundingCircle(10),
+        print self.player.rotation/math.pi
+        colliders = self.player.world.get_colliders(collision.BoundingCone(self.player.bounding_shape.radius + 4,
+                                                                           self.player.rotation,
+                                                                           math.pi/2),
                                                     self.player.position,
-                                                    [self.player])
+                                                    [self.player],
+                                                    objects.Player)
         print "Earth Primary ability collided with %s game objects." % (len(colliders))
