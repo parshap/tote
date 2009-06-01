bd180bd7023bbd8f388aadab7b3e22bd7c0d6d35
Parsha Pourkhomami <parshap+git@gmail.com>
2009-06-01 12:27:03 +0000

    Removed demo-1 branch. Unnecessary as all the changes that were intended to be done here made more sense to be done elsewhere.

--- game/branches/demo-1/gamestate/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is here so that python knows this folder should be treated as a
-# package.
-
-"""
-The gamestate package contains everything relating to the game state.
-"""
-
-import world
-import objects
