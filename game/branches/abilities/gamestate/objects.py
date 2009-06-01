d657a42d02d7406d7cf09032d7dae09dac00d696
Parsha Pourkhomami <parshap+git@gmail.com>
2009-06-01 10:12:28 +0000

    Added ability to change the player's element with F9-F12 keys.

--- game/branches/abilities/gamestate/objects.py
+++ game/branches/abilities/gamestate/objects.py
@@ -301,7 +301,8 @@ class Player(MobileObject):
         # Create an Element and pass it a reference to this player make it our
         # current active element.
         # @todo: don't hardcode this
-        self.element = elements.AirElement(self)
+        self.element = elements.EarthElement(self)
+        self.element_changed = Event()
         
         self.active_abilities = []
         self.last_ability_time = 0
@@ -405,6 +406,21 @@ class Player(MobileObject):
 
         for ability in self.active_abilities:
             ability.update(dt)
+            
+    def change_element(self, element_type):
+        if self.element.type == element_type:
+            return
+        if element_type == "earth":
+            self.element = elements.EarthElement(self)
+        elif element_type == "fire":
+            self.element = elements.FireElement(self)
+        elif element_type == "water":
+            self.element = elements.WaterElement(self)
+        elif element_type == "air":
+            self.element = elements.AirElement(self)
+        else:
+            return
+        self.element_changed(self)
         
     def is_ongcd(self):
         """
