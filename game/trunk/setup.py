8084a172d278ac218ada52c6c5389824bd3bb31c
Parsha Pourkhomami <parshap+git@gmail.com>
2009-06-07 07:45:32 +0000

    Added some other DLLs to ignore when building.

--- game/trunk/setup.py
+++ game/trunk/setup.py
@@ -7,6 +7,8 @@ setup(
         "py2exe": {
             "dll_excludes": [
                 "MSVCP90.dll",
+                "MSVCR90.dll",
+                "boost_python-vc90-mt-1_37.dll",
                 "boost_python-vc90-mt-1_38.dll",
                 "OgreMain.dll"
             ]
