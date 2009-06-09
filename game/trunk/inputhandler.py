0315c5b66a5fafdad4c0826fb479206b7d859bc0
Jeff Gaskill <dirtwizzie@gmail.com>
2009-06-09 00:34:14 +0000

    Fixed left mouse rotation synchronization bug.

--- game/trunk/inputhandler.py
+++ game/trunk/inputhandler.py
@@ -74,6 +74,7 @@ class InputHandler(OIS.MouseListener, OIS.KeyListener):
     def mousePressed(self, event, id):
         if self.scene.player:
             if id == MOUSE_CONTROLS["ABILITY_1"]:
+                self.process()
                 self.scene.player.request_ability(1)
         
         # Inject mouse press to UI elements.
