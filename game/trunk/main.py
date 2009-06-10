6780be6d66c0e1c3573a4c642743c93bd5bd5a34
Parsha Pourkhomami <parshap+git@gmail.com>
2009-06-10 02:38:46 +0000

    Cleaned up main application start up a little bit.

--- game/trunk/main.py
+++ game/trunk/main.py
@@ -20,26 +20,31 @@ def main(argv=None):
 
     # Start the application.
     if len(argv) > 1 and argv[1] == "server":
+        # Run as a server.
         try:
             app = application.ServerApplication()
             app.go()
         except KeyboardInterrupt:
-            app.stop()
+            try: app.stop()
+            except: pass
         except Exception:
             raise
             try: app.stop()
             except: pass
+    
     else:
+        # Run as a client.
+        # Set up the default parameters.
+        player_name = "Newbie"
         address = "localhost"
         port = 8981
-        player_name = "Newbie"
         if len(argv) > 1:
             player_name = argv[1]
         if len(argv) > 2:
             address = argv[2]
         if len(argv) > 3:
             port = int(argv[3])
-        print address, port
+        print "Starting and connecting to %s:%s as %s." % (address, port, player_name)
         try:
             app = application.ClientApplication(player_name, address, port)
             app.go()
